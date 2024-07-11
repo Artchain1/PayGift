fc00975edaa009b6c021a20bf7e6d1358101b6eb
Bayz.ng <ayofe70@gmail.com>
2024-07-11 14:49:15 -0700

    bayo commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
+import Login from './pages/Auth/Login/Login'
+import Register from './pages/Auth/Register/Register'
 // import AuthPage from './pages/AuthPage'
 // import GoogleAuth from './components/authentications/GoogleAuth'
 // import AccountConfirmation from "./components/authentications/AccountConfirmation";
@@ -12,6 +14,8 @@ const App = () => {
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
         {/* <Route path="/auth" element={<AuthPage />} />
 				<Route path="/auth/google-verify" element={<GoogleAuth />} />
 				<Route path="/confirm-email" element={<AccountConfirmation />} />
