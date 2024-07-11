6c4b1ef644c91c0684a712836e8c5e24cbfb7473
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-07-11 18:30:59 +0100

    side commit

---
+++ client/src/components/Dashboard/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import './Dashboard.css'
+
+const Settings = () => {
+  return (
+    <>
+    <Sidebar />
+    <div>Settings</div>
+    </>
+    
+  )
+}
+
+export default Settings
---
+++ client/src/components/Dashboard/Wallet.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Dashboard.css'
+import Sidebar from './Sidebar'
+
+const Wallet = () => {
+  return (
+    <div className='walletCon'>
+        <Sidebar />
+        <div>Wallet</div>
+
+    </div>
+  )
+}
+
+export default Wallet
