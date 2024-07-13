24bb20529e321c52eae6e76e5d30453c918d894a
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-07-13 14:29:10 +0100

    wallet create update

--- client/src/pages/RedeemGiftcardPage.jsx
+++ client/src/pages/RedeemGiftcardPage.jsx
@@ -1,16 +1,17 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import SidebarWithHeader from '../components/SidebarWithHeader'
+import RedeemGiftcard from '../components/Dashboard/RedeemGiftcard'
 
-const RedeemGiftcard = () => {
+const RedeemGiftcardPage = () => {
   return (
     <Box>
         <SidebarWithHeader>
-            
+            <RedeemGiftcard/>
         </SidebarWithHeader>
       
     </Box>
   )
 }
 
-export default RedeemGiftcard
+export default RedeemGiftcardPage
